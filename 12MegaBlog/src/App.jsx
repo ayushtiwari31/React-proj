c06eb4d296c626f3c29be233e2bd445753f7ec99
Ayush Tiwar <ayush.tiwari0031@gmail.com>
2024-01-01 19:21:52 +0530

    appwrite service

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -4,8 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
       <h1>A blog app in appwrite</h1>
